2bbf1817d2d34a4ff38d36e1e86ec02018a9026e
emc <micha3l_255@yahoo.com>
2020-08-03 20:28:55 -0500

    Implement toggle_status

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show] 
-  resources :blogs
+  
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 
   root to: 'pages#home'
   get 'about-me', to: 'pages#about'
